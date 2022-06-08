67ed2f6a92c953d7bb9c9fba83bc4476ad3ca98f
Diamondra Rakotoarinaivo <amada.rktvo@gmail.com>
2022-06-08 11:24:56 +0300

    [IMP] implémentation controller login

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const User = require('../modeles/User');
 const bcrypt = require('bcrypt');
+const Thing = require('../modeles/Thing');
 
 exports.signup = (req, res, next) => {
     bcrypt.hash( req.body.password, 10)
@@ -18,5 +19,22 @@ exports.signup = (req, res, next) => {
 }
 
 exports.login = (req, res, next) => {
-
+    Thing.findOne({ email: req.body.email})
+        .then(user => {
+            if(!user){
+                return res.status(401).json({erreur : 'Utilisateur non trouvé'});
+            }
+            bcrypt.compare(req.body.password, user.password)
+                .then(valide => {
+                    if(!valide){
+                        return res.status(401).json({erreur : 'Mot de passe incorrecte'});
+                    }
+                    res.status(200).json({
+                        userId: user._id,
+                        token: 'TOKEN'
+                    })
+                })
+                .catch(error => res.status(500).json({error}))
+        })
+        .catch(error => res.status(500).json({error}))
 }
