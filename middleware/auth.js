698abe2c9118c3e9a92df4fb748e33154f2dce10
Diamondra RAKOTOARINAIVO <amada.rktvo@gmail.com>
2022-09-05 15:56:43 +0300

    Commit

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 
-module.exports((req, res, next) => {
+module.exports = (req, res, next) => {
     try{
         let token = req.body.authorization.split(' ')[1]; //on split le token car celui-ci sera accompagné de Bearer
         let decodeToken = token.verify(token, 'RANDOM_TOKEN_SECRET_KEY');
@@ -12,4 +12,4 @@ module.exports((req, res, next) => {
     } catch{
         (error) => res.status(401).json({error});
     }
-})
+}
