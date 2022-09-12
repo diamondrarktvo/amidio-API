7705751854b613539899edf33123ba2ad14253b3
diamondra <amada.rktvo@gmail.com>
2022-09-12 17:32:14 +0300

    Rectification bug dans auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 
-module.exports((req, res, next) => {
+module.exports = (req, res, next) => {
     try {
         let token = req.body.authorization.split(" ")[1]; //on split le token car celui-ci sera accompagné de Bearer
         let decodeToken = jwt.verify(token, "RANDOM_TOKEN_SECRET_KEY");
@@ -9,7 +9,7 @@ module.exports((req, res, next) => {
             userId: userId, //on prélève l'userId utilisé dans d'autres controllers ici
         };
         next();
-    } catch {
-        (error) => res.status(401).json({ error });
+    } catch (error) {
+        res.status(400).json({ error });
     }
-});
+};
